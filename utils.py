62b82567c730f631bbf434b192e04ae6e553df5d
Miguel <miguel.garciabohigues@hotmail.com>
2022-10-20 17:29:30 +0200

    task 1

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import math
 import random
 from nltk.tag import hmm
 from nltk.tag import tnt
+import matplotlib.pyplot as plt
 
 #useful functions
 def cosinus_distance(e1, e2):
@@ -35,10 +36,10 @@ def tnt_tagger(train, test):
     tnt_tagger_accuracy = local_tnt_tagger.accuracy(test)
     return tnt_tagger_accuracy
 
-def fold_cross_validation(corpus, fold=10, shuffle=False):
+def fold_cross_validation(corpus, fold=10, shuffle=False, model = 'Both'):
     '''
     function to compute N-fold cross validation over a given corpus with or withour shuffle. 
-    This computes HMM and TNT at the same time
+    model can have the following values: HMM, TNT, Both
     '''
     fold_cross_results = []
     local_corpus = corpus.copy()
@@ -56,7 +57,34 @@ def fold_cross_validation(corpus, fold=10, shuffle=False):
             _train = local_corpus[end:]
         elif end == -1:
             _train = local_corpus[0:start]
-        prec_hmm = hmm_tagger(_train, _test)
-        prec_tnt = tnt_tagger(_train, _test)
-        fold_cross_results.append(np.array([prec_hmm, prec_tnt]))
+        
+        if model == 'HMM':
+            prec_hmm = hmm_tagger(_train, _test)
+            fold_cross_results.append(prec_hmm)
+        if model == 'TNT':
+            prec_tnt = tnt_tagger(_train, _test)
+            fold_cross_results.append(prec_tnt)
+        if model == 'Both':
+            prec_hmm = hmm_tagger(_train, _test)
+            prec_tnt = tnt_tagger(_train, _test)
+            fold_cross_results.append(np.array([prec_hmm, prec_tnt]))
+
     return np.array(fold_cross_results)
+
+def get_num_of_words(fold, corpus):
+    n = 0
+    for sentence in corpus:
+        for word in sentence:
+            n += 1
+    return n//fold
+
+def plot_model(results, ic, model, fold):
+    x=[i+1 for i in range(fold)+1]
+    y=results
+    plt.axis([0, 11, 0.80, 0.97])
+    plt.ylabel('Accuracy')
+    plt.xlabel('Fold')
+    plt.title('Ten-fold cross validation ' + model)
+    plt.plot(x,y,'ro')
+    plt.errorbar(x,y,yerr=ic,linestyle='None')
+    plt.show()
