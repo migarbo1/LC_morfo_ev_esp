d969ea8c1ce6738b519edc7f47f28f4f252cc2d2
TheMuiaio <germamoll@gmail.com>
2022-10-23 12:49:26 +0200

    task1 done

--- task1.py
+++ task1.py
@@ -28,6 +28,8 @@ def cessEsp_10Fold_Hmm():
     n = utils.get_num_of_words(10, corpus)
     ic = [utils.interval_trust(acc,n) for acc in acc_array]
     utils.plot_model(results=acc_array, ic=ic, fold=10, model="HMM")
+    for i in range(len(acc_array)):
+        print(str(acc_array[i]) + " +_ " + str(ic[i]))
 
 
 cessEsp_10Fold_Hmm()
