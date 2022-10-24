47c06972a10dab703c06f228c8df477a59e88e30
Miguel <miguel.garciabohigues@hotmail.com>
2022-10-24 20:13:02 +0200

    fix task1: compare corpus with full categories and with subset

--- task1.py
+++ task1.py
@@ -1,13 +1,15 @@
 import utils as utils
 
 def cessEsp_10Fold_Hmm():
-    corpus = utils.load_corpus()
-    acc_array = utils.fold_cross_validation(corpus, 10, True, 'HMM')
-    n = utils.get_num_of_words(10, corpus)
-    ic = [utils.interval_trust(acc,n) for acc in acc_array]
-    utils.plot_model(results=acc_array, ic=ic, fold=10, model="HMM")
-    for i in range(len(acc_array)):
-        print(str(acc_array[i]) + " +_ " + str(ic[i]))
+    for value in [True, False]:
+        corpus = utils.load_corpus(value)
+        acc_array = utils.fold_cross_validation(corpus, 10, True, 'HMM')
+        n = utils.get_num_of_words(10, corpus)
+        ic = [utils.interval_trust(acc,n) for acc in acc_array]
+        utils.plot_model(results=acc_array, ic=ic, fold=10, model="HMM")
+        print('reduce corpus tags: {}'.format(value))
+        for i in range(len(acc_array)):
+            print(str(acc_array[i]) + " +_ " + str(ic[i]))
 
 
 cessEsp_10Fold_Hmm()
