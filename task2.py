a1196d2eb8be6ba8f9d6d4523e61a75e54b1f2c1
Miguel <miguel.garciabohigues@hotmail.com>
2022-10-24 20:13:39 +0200

    fix task2: change in title, removed from folds the test set

--- task2.py
+++ task2.py
@@ -5,7 +5,7 @@ def cessEsp_incremental_Hmm():
     acc_array = utils.incremental_test(corpus, 10, True, 'HMM')
     n = utils.get_num_of_words(10, corpus)
     ic = [utils.interval_trust(acc,n) for acc in acc_array]
-    utils.plot_model(results=acc_array, ic=ic, fold=10, model="HMM")
+    utils.plot_model(results=acc_array, ic=ic, fold=9, title='Incremental validation', model="HMM")
     for i in range(len(acc_array)):
         print(str(acc_array[i]) + " +- " + str(ic[i]))
 
